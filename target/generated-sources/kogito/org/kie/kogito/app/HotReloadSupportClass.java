7cc8b40ff70668641651249badb854dec857b6eb
NikitaPorwal21 <porwal.nikita@gmail.com>
2023-11-07 13:39:18 +0530

    Updated the code

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "f034b6d5-5c61-4d1f-b576-91b6567bf3e6";
+private static final String ID = "c886405f-b47b-412d-8a49-d6dd5e9a481f";
 }
